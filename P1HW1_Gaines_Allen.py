71fc9495f1bce3a198511b997a828b4463984e88
Allen-Gaines <allenjgaines@gmail.com>
2024-09-16 11:51:02 -0400

    P1HW1
    Learning Math in python

---
+++ P1HW1_Gaines_Allen.py
@@ -0,0 +1,20 @@
+#Gaines Allen
+#9/16/2024
+#use python to code an output
+
+
+
+
+print("----Calculating Exponenets----")
+Base_value=int(input("enter interger as the base value: "))
+Exp=int(input("enter an integer as an exponet:"))
+
+Answer=Base_value**Exp
+
+print( Base_value,"raised to to the power of",Exp, "is", Answer)
+
+print("--------Addition and subtraction-----")
+
+Starint= int(input(enter a starting
+
+
