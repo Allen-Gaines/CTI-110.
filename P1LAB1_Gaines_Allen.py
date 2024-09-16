c6f664138c3305b4b0e0e88b039b6b30d3a7ecde
Allen-Gaines <allenjgaines@gmail.com>
2024-09-16 11:24:15 -0400

    First Upload

---
+++ P1LAB1_Gaines_Allen.py
@@ -0,0 +1,13 @@
+# Allen Gaines#
+#9/16/2024#
+#P1Lab1#
+#Write your code in IDLE. Make sure you go to File > New File to create a new program#
+
+#get the first name 
+Last_name=input("Enter last name: ")
+#get the last name
+First_Name=input("enter first name: ")
+#display info to user
+print("Hello,",First_Name, Last_name+"!","Welcome to CTI-110")
+
+                
