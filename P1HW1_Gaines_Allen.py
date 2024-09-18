45ca44ce6342c3a6bff486e69c5994a82ce8eb1e
Allen-Gaines <allenjgaines@gmail.com>
2024-09-18 10:27:48 -0400

    Update P1HW1_Gaines_Allen.py

--- P1HW1_Gaines_Allen.py
+++ P1HW1_Gaines_Allen.py
@@ -1,20 +1,27 @@
 #Gaines Allen
 #9/16/2024
-#use python to code an output
+#use python to code a mathmatical output 
 
 
 
 
 print("----Calculating Exponenets----")
 Base_value=int(input("enter interger as the base value: "))
-Exp=int(input("enter an integer as an exponet:"))
+Exp=int(input("enter an integer as an exponet: "))
 
 Answer=Base_value**Exp
 
-print( Base_value,"raised to to the power of",Exp, "is", Answer)
+print( Base_value,"raised to to the power of",Exp, "is", Answer,"!!")
 
 print("--------Addition and subtraction-----")
 
-Starint= int(input(enter a starting
+Starint= int(input("enter a starting integere: "))
 
+
+Int1=int(input("enter an integer to add: "))
+Int2= int (input("enter an integer to subtract: "))
+
+Answer2=Starint+ Int1 - Int2
+
+print(Starint,"+",Int1,"-", Int2,"is equal to", Answer2)
 
