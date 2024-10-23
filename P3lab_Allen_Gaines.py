cf31c3f901d76141833cf8537d6692045989f505
Allen-Gaines <allenjgaines@gmail.com>
2024-10-23 10:37:52 -0400

    Add files via upload

--- P3lab_Allen_Gaines.py
+++ P3lab_Allen_Gaines.py
@@ -20,17 +20,20 @@ money = float(input("Enter the amount of money as a float: $"))
 # convert money as a whole number
 
 money= round(money * 100)
+
 #money= int(money * 100) may need to round this value money= round(money*100) 
-print(money)
+#print(money)
 #get whole dollars for money ammount 
 Dollars = money//100
-print(f"Dollars: {Dollars}")
-print(Dollars)
+#print(f"Dollars: {Dollars}")
+#print(Dollars)
 # remove dollar ammount from money
 money= money - (Dollars * 100)
-
-print(money)
-
+if money == 0:
+    print("no change")
+#print(money)
+Dollars = money//100
+print(f"Dollars: {Dollars}")
 #get quarters
 Quarters = money // 25
 print (f" Quarters: {Quarters}")
@@ -47,5 +50,23 @@ money= money - (nickels * 5)
 pennies= money
 print(f"pennies: {pennies}")
 
-
-
+if Dollars >= 1:
+    if Dollars == 1:
+        print (f"{Dollars} Dollar")
+    else: print(f"{Dollars} Dollars")
+if Quarters >= 1:
+    if Quarters == 1:
+        print (f"{Quarters} Quarter")
+    else: print(f"{Quarters} Quarters")
+if dimes >= 1:
+    if dimes == 1:
+        print (f"{dimes} Dime")
+    else: print(f"{dimes} Dims")
+if nickels >= 1:
+    if nickels == 1:
+        print (f"{nickels} Nickel")
+    else: print(f"{nickels} Nickels")
+if pennies >= 1:
+    if pennies == 1:
+        print (f"{pennies} Pennie")
+    else: print(f"{pennies} Pennies")
