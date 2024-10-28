07f903c4cf3c0acaa8b97743145ca2ab9e6aaafb
Allen-Gaines <allenjgaines@gmail.com>
2024-10-28 11:50:03 -0400

    P4Lab1

---
+++ P4Lab1_Gaines_Allen.py
@@ -0,0 +1,34 @@
+#Allen Gaines
+# P4Lab1
+# 10/28/2024
+# use loops to make a square using turtle
+
+
+import turtle
+
+win = turtle.Screen()
+t= turtle.Turtle()
+
+
+t.pensize(4)
+t.pencolor("blue")
+t.shape("arrow")
+
+
+line= 0 
+while line <4:
+    t.right(90)
+    t.forward(150)
+    line+= 1
+
+for line1 in range(3):
+    t.left(120)
+    t.forward(150)
+    
+
+
+
+
+    
+
+    
